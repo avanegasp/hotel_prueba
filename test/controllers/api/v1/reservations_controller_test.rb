771ac0a6abb8b24a7ba7dc2eeb4d04c48ec99eeb
Angie <arco527@gmail.com>
2019-02-04 17:54:01 -0500

    Testing controller

--- test/controllers/api/v1/reservations_controller_test.rb
+++ test/controllers/api/v1/reservations_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class Api::V1::ReservationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test "get index: is successful" do
+      get api_v1_reservations_path
+    assert_response :success
+  end
+
+  test "reservation create: creates an reservation"
+    assert_difference 'Reservation.count', 1 do
+      post api_v1_reservations_path, params: { reservation: { name: "Alejo", published: false, last_name:"Escobar", loyalty_member: false, room_id:1 } }
+  end
 end
